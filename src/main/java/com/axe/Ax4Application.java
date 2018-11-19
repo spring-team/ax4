921aa8ee3ea46150523b02734d00875bf01adbae
Rod Johnson <johnsonroda@gmail.com>
2018-11-19 14:58:16 +0100

    Initial commit from Atomist

---
+++ src/main/java/com/axe/Ax4Application.java
@@ -0,0 +1,12 @@
+package com.axe;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ax4Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Ax4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/axe/Ax4ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.axe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ax4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
